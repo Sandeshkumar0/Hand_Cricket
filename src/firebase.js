ddf19fc64a1f4aee97b4141ff734f4e04c70c7f0
sandeshkumar0 <sandeshkumar07@gmail.com>
2026-07-22 21:47:25 +0530

    chore: update firebase realtime database link to hand-cricket-f9e44

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,26 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBdZqr8EClhENZ4aoobxzhMPS_Ytg30nlg",
-  authDomain: "hand-cricket-3a716.firebaseapp.com",
-  databaseURL: "https://hand-cricket-3a716-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "hand-cricket-3a716",
-  storageBucket: "hand-cricket-3a716.firebasestorage.app",
-  messagingSenderId: "847333952255",
-  appId: "1:847333952255:web:bafc591d5126744982daca"
+  apiKey: "AIzaSyD36fDZ3QyNcLsVewVhr1qkFzDUPWtuYsQ",
+  authDomain: "hand-cricket-f9e44.firebaseapp.com",
+  databaseURL: "https://hand-cricket-f9e44-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "hand-cricket-f9e44",
+  storageBucket: "hand-cricket-f9e44.firebasestorage.app",
+  messagingSenderId: "430359067764",
+  appId: "1:430359067764:web:d5c21bddcf44f08b8b6521",
+  measurementId: "G-Y0ZF3QDZ66"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 export const db = getDatabase(app);
 export const auth = getAuth(app);
